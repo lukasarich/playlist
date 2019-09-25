a624f4b4d4b76d72ccd9d79c302143eb67da274e
Luka Sarich <lukasarichhh@gmail.com>
2019-09-25 16:12:04 +0200

    Updated

--- js/playlist.js
+++ js/playlist.js
@@ -1,10 +1,8 @@
-prompts = prompt("Is this working?");
-
 var playList = [
   ['I Did It My Way', 'Frank Sinatra'],
   ['Respect', 'Aretha Franklin'],
   ['Imagine', 'John Lennon'],
-  ['Born To Run', 'Bruce Springsteen'],
+  ['Born to Run', 'Bruce Springsteen'],
   ['Louie Louie', 'The Kingsmen'],
   ['Maybellene', 'Chuck Berry']
 ];
@@ -13,10 +11,10 @@ function print(message) {
   document.write(message);
 }
 
-function printSongs(songs) {
+function printSongs( songs ) {
   var listHTML = '<ol>';
   for ( var i = 0; i < songs.length; i += 1) {
-    listHTML += '<li>' + songs[i][0] + ' by ' songs[i][1] + '</li>';
+    listHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>';
   }
   listHTML += '</ol>';
   print(listHTML);
